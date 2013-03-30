06e974a87085db856561b1929bd45933dc2ef3e4
Javier Angulo Lucerón <javier.angulo1@gmail.com>
2013-03-30 20:21:32 +0100

    Project created with autotools for the build process and dummy main file

---
+++ src/main.cpp
@@ -0,0 +1,17 @@
+/* 
+ * main.cpp -- Main file of the project with system initialization
+ *
+ * Copyright (C) 2013 Javier Angulo Lucerón <javier.angulo1@gmail.com>
+ * 
+ * This file is part of CamelRace
+ *
+ * CamelRace is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+ * CamelRace is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License along with CamelRace.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+int
+main(int argc, char **argv)
+{
+	return 0;
+}
